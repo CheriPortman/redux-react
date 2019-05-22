005c3b941208c338f20b2332e14c34be8dd644c1
Cheri Portman <cheriportman@hotmail.com>
2019-05-22 15:01:08 -0700

    items removed from store

--- src/index.js
+++ src/index.js
@@ -11,12 +11,20 @@ function reducer(state = initialState, action) {
   switch(action.type) {
     case 'ADD_DRINK':
       return { ...state, drink: action.payload };
+    case 'REMOVE_DRINK':
+      return { ...state, drink: null };
     case 'ADD_SANDWICH':
       return { ...state, sandwich: action.payload };
+    case 'REMOVE_SANDWICH':
+      return { ...state, sandwich: null };
     case 'ADD_FRUIT':
       return { ...state, fruit: action.payload };
+    case 'REMOVE_FRUIT':
+      return { ...state, fruit: null };
     case 'ADD_DESSERT':
       return { ...state, dessert: action.payload };
+    case 'REMOVE_DESSERT':
+      return { ...state, dessert: null };
     default: 
       return state;
   }
@@ -52,4 +60,27 @@ store.dispatch({
 
 console.log('dessert', store.getState());
 
+store.dispatch({
+  type: 'REMOVE_DRINK'
+});
+
+console.log('removed drink', store.getState());
+
+store.dispatch({
+  type: 'REMOVE_SANDWICH'
+});
+
+console.log('sandwith removed', store.getState());
+
+store.dispatch({
+  type: 'REMOVE_FRUIT'
+});
+
+console.log('fruit removed', store.getState());
+
+store.dispatch({
+  type: 'REMOVE_DESSERT'
+});
+
+console.log('fruit removed', store.getState());
 
