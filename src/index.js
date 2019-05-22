73089f232c169bc7d53cf26d87c03735faa2a41c
Cheri Portman <cheriportman@hotmail.com>
2019-05-22 14:51:11 -0700

    can add items to the store

--- src/index.js
+++ src/index.js
@@ -31,4 +31,25 @@ store.dispatch({
 
 console.log('add drink', store.getState());
 
+store.dispatch({
+  type: 'ADD_SANDWICH',
+  payload: 'liverwurst'
+});
+
+console.log('sandwich', store.getState());
+
+store.dispatch({
+  type: 'ADD_FRUIT',
+  payload: 'apple'
+});
+
+console.log('fruit', store.getState());
+
+store.dispatch({
+  type: 'ADD_DESSERT',
+  payload: 'cookies'
+});
+
+console.log('dessert', store.getState());
+
 
