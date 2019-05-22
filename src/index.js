e54497621ab71216eb10b6abdce27b5289b6bb35
Cheri Portman <cheriportman@hotmail.com>
2019-05-22 15:10:14 -0700

    cleanup code a bit

--- src/index.js
+++ src/index.js
@@ -36,51 +36,43 @@ store.dispatch({
   type: 'ADD_DRINK',
   payload: 'Bubbly'
 });
-
 console.log('add drink', store.getState());
 
 store.dispatch({
   type: 'ADD_SANDWICH',
   payload: 'liverwurst'
 });
-
 console.log('sandwich', store.getState());
 
 store.dispatch({
   type: 'ADD_FRUIT',
   payload: 'apple'
 });
-
 console.log('fruit', store.getState());
 
 store.dispatch({
   type: 'ADD_DESSERT',
   payload: 'cookies'
 });
-
 console.log('dessert', store.getState());
 
 store.dispatch({
   type: 'REMOVE_DRINK'
 });
-
 console.log('removed drink', store.getState());
 
 store.dispatch({
   type: 'REMOVE_SANDWICH'
 });
-
 console.log('sandwith removed', store.getState());
 
 store.dispatch({
   type: 'REMOVE_FRUIT'
 });
-
 console.log('fruit removed', store.getState());
 
 store.dispatch({
   type: 'REMOVE_DESSERT'
 });
-
 console.log('fruit removed', store.getState());
 
