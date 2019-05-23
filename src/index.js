9d5eb8e334bdd13dac13da05df46c7c5c506b0af
Cheri Portman <cheriportman@hotmail.com>
2019-05-22 17:29:14 -0700

    reducer pulled into own component

--- src/index.js
+++ src/index.js
@@ -1,39 +1,37 @@
 import { createStore } from 'redux';
-import { addDrink, removeDrink, 
-  addSandwich, removeSandwich, 
-  addFruit, removeFruit,
-  addDessert, removeDessert } 
-  from './actions/lunchActions';
+import reducer from './reducers/lunchReducer';
+import { addDrink, removeDrink, addSandwich, removeSandwich, 
+  addFruit, removeFruit, addDessert, removeDessert } from './actions/lunchActions';
 
-const initialState = {
-  drink: null,
-  sandwich: null,
-  fruit: null,
-  dessert: null
-};
+// const initialState = {
+//   drink: null,
+//   sandwich: null,
+//   fruit: null,
+//   dessert: null
+// };
 
-function reducer(state = initialState, action) {
-  switch(action.type) {
-    case 'ADD_DRINK':
-      return { ...state, drink: action.payload };
-    case 'REMOVE_DRINK':
-      return { ...state, drink: null };
-    case 'ADD_SANDWICH':
-      return { ...state, sandwich: action.payload };
-    case 'REMOVE_SANDWICH':
-      return { ...state, sandwich: null };
-    case 'ADD_FRUIT':
-      return { ...state, fruit: action.payload };
-    case 'REMOVE_FRUIT':
-      return { ...state, fruit: null };
-    case 'ADD_DESSERT':
-      return { ...state, dessert: action.payload };
-    case 'REMOVE_DESSERT':
-      return { ...state, dessert: null };
-    default: 
-      return state;
-  }
-}
+// function reducer(state = initialState, action) {
+//   switch(action.type) {
+//     case 'ADD_DRINK':
+//       return { ...state, drink: action.payload };
+//     case 'REMOVE_DRINK':
+//       return { ...state, drink: null };
+//     case 'ADD_SANDWICH':
+//       return { ...state, sandwich: action.payload };
+//     case 'REMOVE_SANDWICH':
+//       return { ...state, sandwich: null };
+//     case 'ADD_FRUIT':
+//       return { ...state, fruit: action.payload };
+//     case 'REMOVE_FRUIT':
+//       return { ...state, fruit: null };
+//     case 'ADD_DESSERT':
+//       return { ...state, dessert: action.payload };
+//     case 'REMOVE_DESSERT':
+//       return { ...state, dessert: null };
+//     default: 
+//       return state;
+//   }
+// }
 
 const store = createStore(reducer);
 
