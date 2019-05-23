e4d56b3852e93833e988172c1eb0b4d9a85f64c8
Cheri Portman <cheriportman@hotmail.com>
2019-05-23 12:03:27 -0700

    added combineReducers

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import { combineReducers } from 'redux';
+import post from './reducers/postReducer';
+import comment from './reducers/commentReducer';
 
+export default combineReducers({
+  post,
+  comment
+});
