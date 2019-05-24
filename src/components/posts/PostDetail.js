c7a099a56a66b1393a85f49eb2904ed93dfd4037
Cheri Portman <cheriportman@hotmail.com>
2019-05-24 11:28:01 -0700

    wrote PostById.js

--- src/components/posts/PostDetail.js
+++ src/components/posts/PostDetail.js
@@ -1,4 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import CommentsByPostId from '../../container/comments/CommentsByPostId';
-import CreateComment from '../../containers/comments/CreateComment'
+import CommentsByPostId from '../../container/comments/CommentsByPostId';
+import CreateComment from '../../containers/comments/CreateComment';
+
+function PostDetail({ post }) {
+  const paras = post.body.split('\n').map((p, i) => (
+    <p key={i}>{p}</p>
+  ));
+
+  return (
+    <section>
+      <h1>{post.title}</h1>
+      {paras}
+      <CommentsByPostId postId={post.id} />
+      <CreateComment postId={post.id} />
+    </section>
+  );
+}
+
+PostDetail.propTypes = {
+  post: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    body: PropTypes.shape.isRequired
+  }).isRequired
+};
+
+export default PostDetail;
