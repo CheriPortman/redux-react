42640b97eb95094124492ae402e873f69f9b6ef2
Cheri Portman <cheriportman@hotmail.com>
2019-05-24 08:17:07 -0700

    Wrote Comment.js

---
+++ src/components/posts/PostDetail.js
@@ -0,0 +1,4 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import CommentsByPostId from '../../container/comments/CommentsByPostId';
+// import CreateComment from '../../containers/comments/CreateComment'
